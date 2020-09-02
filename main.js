fb2be10d4a6c4df4d91b9e236fac3f59a98d48d9
Flavia <flaviamatta90@gmail.com>
2020-09-02 10:35:35 +0200

    3 different mode
    addClass, show, fadeIn

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
-// mostro il menù principale clickando sull'hamburger
-$(".header-right > a").click(
+// // mostro il menù principale clickando sull'hamburger
+// $(".header-right > a").click(
+//   function() {
+//   $(".hamburger-menu").addClass("active");
+// }
+// );
+//
+// // nascondo il menù principale clickando sulla x
+// $(".close").click(
+//   function() {
+//   $(".hamburger-menu").removeClass("active");
+// }
+// );
+
+
+// $(".header-right .fas.fa-bars").click(
+//   function() {
+//   $(".hamburger-menu").show(2000, miaFunzione);
+//   }
+// );
+//
+// $(".hamburger-menu .close").click(
+//   function() {
+//   $(".hamburger-menu").hide();
+// }
+// );
+//
+// function miaFunzione(){
+//   alert("Ora il menù è visibile")
+// };
+
+
+$(".header-right .fas.fa-bars").click(
   function() {
-  $(".hamburger-menu").addClass("active");
-}
+  $(".hamburger-menu").fadeIn(2000);
+  }
 );
 
-// nascondo il menù principale clickando sulla x
-$(".close").click(
+$(".hamburger-menu .close").click(
   function() {
-  $(".hamburger-menu").removeClass("active");
+  $(".hamburger-menu").fadeOut();
 }
 );
